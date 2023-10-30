7ea42a94115fc63b4a4dcb3462ab6242d1517be8
devthuan <devthuan24@gmail.com>
2023-10-30 11:11:05 +0700

    create branch thuan

---
+++ src/model/Product.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Product extends BaseEntity {
+    private String name;
+    private double price;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
