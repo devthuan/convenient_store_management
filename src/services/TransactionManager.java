be65fa98c9000f1e2d3c3b9243461ebc582f6b60
UchiTan <tdangphuc902@gmail.com>
2023-11-07 20:13:39 -0900

    Sua loi gium tui voi tui mua cafe cho

--- src/services/TransactionManager.java
+++ src/services/TransactionManager.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import model.Customer;
 import model.InterfaceCRUD;
+import model.Product;
 import model.Transaction;
-
+import repository.CustomerRepository;
+import repository.TransactionReponsitory;
 import ui.TransactionUI;
 
 public class TransactionManager implements InterfaceCRUD<Transaction> {
@@ -16,6 +19,31 @@ public class TransactionManager implements InterfaceCRUD<Transaction> {
         public static void startTransactionManager(Scanner scanner) {
                 TransactionUI.handleTransaction(scanner, transactions);
         }
+        public static void saveFile() {
+        TransactionRepository.writeTransactionToFile(transactions, file_path);
+        transactions.clear();
+
+    }
+    public static void readFile() {
+        List<Transaction> transactions_in_file = TransactionRepository.readFileTransaction(file_path);
+        transactions.clear();
+        if (transactions_in_file != null) {
+            for (Transaction transaction : transactions_in_file) {
+                transactions.add(transaction);
+            }
+
+        }
+    }
+    public static void exportTransaction(Transaction transaction) {
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Mã giao dịch                 : " + transaction.getTransactionId());
+        System.out.println("Tên thu ngân                      : " + transaction.getEmployee());
+        System.out.println("Tên khách hàng                    : " + transaction.getCustomer());
+        System.out.println("Tổng số tiền                   : " + transaction.getTotalAmount());
+        System.out.println("Ngày giao dịch              :" + transaction.getTransactionDate());
+        System.out.println("Phương thức thanh toán       : " + transaction.getPaymentMethod());
+        System.out.println("--------------------------------------------------------------");
+    }
 
         public static void exportAllTransaction(List<Transaction> transactions) {
                 if (transactions != null) {
@@ -48,34 +76,62 @@ public class TransactionManager implements InterfaceCRUD<Transaction> {
                 }
         }
 
+
         @Override
-        public void create(Transaction entity) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'create'");
+        public void create(Transaction transaction) {
+
+                transactions.add(transaction);
         }
 
         @Override
         public Transaction read(int id) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'read'");
+               for (Transaction transaction : transactions) {
+                if (transaction.getId() == id) {
+                        return transaction;
+                }
+                
+               } 
+                
         }
 
         @Override
-        public void update(int id, Transaction entity) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'update'");
+        public void update(int id, Transaction updated_transaction) {
+                for (int i = 0; i < transactions.size(); i++) {
+                        Transaction transaction = transactions.get(i);
+                        if (transaction.getId() == id) {
+                            transaction.setTransactionId(updated_transaction.getTransactionId());
+                            transaction.setTransactionDate(updated_transaction.getTransactionDate());
+                            transaction.setTotalAmount(updated_transaction.getTotalAmount());
+                            transaction.setPaymentMethod(updated_transaction.getPaymentMethod());
+                            transaction.setCustomer(updated_transaction.getCustomer());
+                            transaction.setEmployee(updated_transaction.getEmployee());
+                            // Cập nhật thông tin sản phẩm dựa trên ID
+                            return;
+                        }
+                    }
+                
         }
 
         @Override
         public void delete(int id) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'delete'");
+                for (int i = 0; i < transactions.size(); i++) {
+                        if (transactions.get(i).getId() == id) {
+                            transactions.remove(i);
+                            System.out.println("Đã xóa giao dịch thành công!");
+            
+                        }
+                    }
+                    System.out.println("Không tìm thấy giao dịch có mã: " + id);
         }
 
         @Override
         public Transaction search(int id) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'search'");
+                for (Transaction transaction : transactions) {
+            if (transaction.getId() == id) {
+                return transaction;
+            }
+        }
+        return null;
         }
 
 }
